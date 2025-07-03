001fd69f3959a0e32a73639ecb47b05e3915a09b
WebRizen <webrizen@gmail.com>
2025-07-03 16:35:25 +0530

    added

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,18 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  async headers() {
+    return [
+      {
+        source: "/(.*)", // Apply to all routes
+        headers: [
+          {
+            key: "X-Robots-Tag",
+            value: "index, follow",
+          },
+        ],
+      },
+    ];
+  },
+};
 
 export default nextConfig;
